23e81df99a37c84badbff1cc1e00315fbdd433f3
tkaciuk <itcaciuc@pycoding.biz>
2017-09-26 19:24:24 +0300

    Responsive (#21)

--- js/main.js
+++ js/main.js
@@ -9,6 +9,62 @@ jQuery(function($) {
 
 });
 
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar-fix').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+    if ($(window).scrollTop() > 50) {
+        $('.navbar-fix').addClass('color-nav');
+    }
+    else {
+        $('.navbar-fix').removeClass("color-nav");
+    }
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        
+        $('.navbar-fix').removeClass('nav-down').addClass('nav-up');
+    } else {
+        
+        if(st + $(window).height() < $(document).height()) {
+            $('.navbar-fix').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+
+
+$('.icon-burger').click(function () {
+    $(this).toggleClass('open').toggleClass('icon-burger').toggleClass('icon-arrow');
+
+    if ( $('.icon-arrow').hasClass('open')) {
+        $('html').css('overflow','hidden');
+         $('.navbar-fix').addClass('color-nav-responsive');
+    }
+    else {
+        $('html').css('overflow', 'scroll');
+        $('.navbar-fix').removeClass('color-nav-responsive');
+    }
+});
 
 $(function () {
     var $content = $('#jsonContent');
