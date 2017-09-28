bf239e25f44a6c1bf97091ef6b6d5b5a0ed93fe3
tkaciuk <itcaciuc@pycoding.biz>
2017-09-28 19:49:01 +0300

    #MitocGroup Contact Page + Anchors for Main Page (#23)
    * Contact Page
    
    * Hot fix

--- js/main.js
+++ js/main.js
@@ -109,4 +109,15 @@ $(function () {
             $content.html(output);
         }
     });
+
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
 });
