9f5b32ec60ab835de442a1596c9fcc09d2a9a3b2
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-10-03 20:47:53 +0300

    Confirnation page for MailChiml forms

--- js/modal-effects.js
+++ js/modal-effects.js
@@ -7,26 +7,28 @@ $(function() {
 
   let startProjectContent = `<div class="md-content popup">
       <h3>Start a Project</h3>
-      <div id="mc_embed_signup">
+      <div id="mc_embed_signup contact-form">
           <form action="//mitocgroup.us11.list-manage.com/subscribe/post?u=13a7a5fca813b378c24ec9fe3&amp;id=f6629ecf38" 
                 method="post" 
-                id="mc-embedded-subscribe-form" 
+                id="mc-embedded-subscribe-form-modal" 
                 name="mc-embedded-subscribe-form"
-                data-redirect-path="about"
-                class="validate" 
+                data-redirect-path="/confirm/start-project"
+                class="validate form-styles" 
                 novalidate>
               <div id="mc_embed_signup_scroll">
                   <div class="flex-row">
-                      <div class="flex-item-6">
+                      <div class="flex-item-12">
                           <div class="mc-field-group input-styles space-input">
                               <label for="mce-FNAME" class="mc-response-label"></label>
                               <input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="Full Name" required>
                           </div>
                       </div>
-                      <div class="flex-item-6">
+                      </div>
+                      <div class="flex-row">
+                      <div class="flex-item-12">
                           <div class="mc-field-group input-styles">
-                              <label for="mce-EMAIL" class="mc-response-label"></label>
                               <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email" required>
+                              <label for="mce-EMAIL" class="mc-response-label"></label>
                           </div>
                       </div>
                       <label class="mc-response-label"></label>
@@ -44,7 +46,7 @@ $(function() {
                       <button class="btn-popup close-start-project-modal">Cancel</button>
                   </div>
                   <div class="flex-item-6">
-                      <button class="btn-popup" type="submit" name="subscribe" id="mc-embedded-subscribe">Submit</button>
+                      <button class="btn-popup submit-modal-form" type="submit" name="subscribe" id="mc-embedded-subscribe">Submit</button>
                   </div>
               </div>
           </form>
@@ -53,32 +55,36 @@ $(function() {
 
   let becomePartnerContent = `<div class="md-content popup">
       <h3>Become a Partner</h3>
-      <div id="mc_embed_signup">
+      <div id="mc_embed_signup contact-form">
           <form action="//mitocgroup.us11.list-manage.com/subscribe/post?u=13a7a5fca813b378c24ec9fe3&amp;id=7257663d85" 
                 method="post" 
-                id="mc-embedded-subscribe-form" 
+                id="mc-embedded-subscribe-form-modal" 
                 name="mc-embedded-subscribe-form" 
-                class="validate"
-                data-redirect-path="contact" 
+                class="validate form-styles"
+                data-redirect-path="/confirm/become-partner" 
                 novalidate>
               <div id="mc_embed_signup_scroll">
                   <div class="flex-row">
-                      <div class="flex-item-4">
+                      <div class="flex-item-12">
                           <div class="mc-field-group input-styles">
                               <label for="mce-FNAME" class="mc-response-label"></label>
                               <input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="Full Name">
                           </div>
                       </div>
-                      <div class="flex-item-4">
+                      </div>
+                      <div class="flex-row">
+                      <div class="flex-item-12">
                           <div class="mc-field-group input-styles">
                               <label for="mce-COMPANY"></label>
-                              <input type="text" value="" name="COMPANY" class="required" id="mce-COMPANY">
+                              <input type="text" value="" name="COMPANY" class="required" id="mce-COMPANY" placeholder="Company">
                           </div>
                       </div>
-                      <div class="flex-item-4">
+                      </div>
+                      <div class="flex-row">
+                      <div class="flex-item-12">
                           <div class="mc-field-group input-styles">
-                              <label for="mce-EMAIL" class="mc-response-label"></label>
                               <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email">
+                              <label for="mce-EMAIL" class="mc-response-label"></label>
                           </div>
                       </div>
                   </div>
@@ -91,7 +97,7 @@ $(function() {
                       <button class="btn-popup close-start-project-modal">Cancel</button>
                   </div>
                   <div class="flex-item-6">
-                      <button class="btn-popup" type="submit" name="subscribe" id="mc-embedded-subscribe">Submit</button>
+                      <button class="btn-popup submit-modal-form" type="submit" name="subscribe" id="mc-embedded-subscribe">Submit</button>
                   </div>
               </div>
           </form>
@@ -109,8 +115,8 @@ $(function() {
     becomePartner.close();
   });
 
-  $body.on('click', '#mc-embedded-subscribe', function () {
-    let $form = $('#mc-embedded-subscribe-form');
+  $body.on('click', '.submit-modal-form', function () {
+    let $form = $('#mc-embedded-subscribe-form-modal');
 
     $(this).on('click', function () {
       $form.submit();
