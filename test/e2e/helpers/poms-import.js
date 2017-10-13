a363a8b0a0f9c15c2f6407018edf7ccf65c2d01f
avozicov <avozicov@pycoding.biz>
2017-10-13 16:15:22 +0300

    Fixing poms import

--- test/e2e/helpers/poms-import.js
+++ test/e2e/helpers/poms-import.js
@@ -1,8 +1,5 @@
 import { HomePage } from '../poms/pages/home.po'
-<<<<<<< HEAD
 import { Header, TopMenu } from '../poms/components/header.po'
-=======
->>>>>>> af2d694931a6e729045a1f17ef7ed482f1d3b22a
 import { StartProject, ContactUsForm, BecomePartner } from '../poms/components/forms.po'
 
 module.exports = {
