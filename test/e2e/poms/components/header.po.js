9130f0c36fae0cda655a4807408f5b68be2bb0a3
Alexandr Vozicov <avozicov@mitocgroup.com>
2017-10-13 16:44:59 +0300

    Adding e2e scripts with links and path assertions (#66)
    * Enabling e2e_tests for Travis execution
    
    * Enabling e2e_tests for Travis execution
    
    * Fix travis.yml to debug e2e execution
    
    * Enabling run e2e script
    
    * Refactored deploy.sh in order to not use --profile when executing in Travis
    
    * Fixing deploy.sh
    
    * Changing css styles to check if deploy.sh works as expected after changes
    
    * Debuging Travis execution
    
    * Debuging Travis execution
    
    * Debuging Travis execution
    
    * Debuging Travis execution
    
    * Debuging Travis execution
    
    * Fixing deploy.sh and init.sh scripts
    
    * Fixing codeclimate issues and refactoring config
    
    * Adding node library for url resolve
    
    * Fixing eslint errors
    
    * Adding scripts for form submit
    
    * Define fixture and test as global variable
    
    * Adding e2e scripts with links and path assertions, fixing poms class after css changes
    
    * Fixing poms import

--- test/e2e/poms/components/header.po.js
+++ test/e2e/poms/components/header.po.js
@@ -9,11 +9,11 @@ export class Header {
 
 export class TopMenu {
   constructor () {
-    this.services = Selector('body > div > div.main-page > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(1) > a', isVisible)
-    this.partners = Selector('body > div > div.main-page > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(2) > a', isVisible)
-    this.customers = Selector('body > div > div.main-page > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(3) > a', isVisible)
-    this.abouts = Selector('body > div > div.main-page > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(4) > a', isVisible)
-    this.contact = Selector('body > div > div.main-page > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(5) > a', isVisible)
-    this.blog = Selector('body > div > div.main-page > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(6) > a', isVisible)
+    this.services = Selector('body > div > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(1) > a', isVisible)
+    this.partners = Selector('body > div > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(2) > a', isVisible)
+    this.customers = Selector('body > div > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(3) > a', isVisible)
+    this.about = Selector('body > div > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(4) > a', isVisible)
+    this.contact = Selector('body > div > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(5) > a', isVisible)
+    this.blog = Selector('body > div > div.navbar-fix.nav-down > div > div.nav-bar > ul > li:nth-child(6) > a', isVisible)
   }
 }
---
+++ test/e2e/suite/pages/home.e2e.js
@@ -0,0 +1,60 @@
+/* global fixture, test */
+
+import url from 'url'
+import { config, sharedFunctions } from '../../helpers/config-import'
+import { Header, TopMenu } from '../../helpers/poms-import'
+
+const header = new Header()
+const topMenu = new TopMenu()
+
+const fix = fixture`Check 'Home' page content`
+  .page`${url.resolve(config.www_base_host, '/')}`
+
+sharedFunctions.windowResolution(fix)
+
+test('Check "Logo" image is displayed on website header', async t => {
+  await t
+    .expect(header.logo)
+})
+
+test('Check "Services" top-menu link is displayed on website header', async t => {
+  await t
+    .expect(topMenu.services.innerText).match(
+    sharedFunctions.anyCase('Services')
+    )
+})
+
+test('Check "Services" top-menu link is displayed on website header', async t => {
+  await t
+    .expect(topMenu.partners.innerText).match(
+    sharedFunctions.anyCase('Partners')
+    )
+})
+
+test('Check "Services" top-menu link is displayed on website header', async t => {
+  await t
+    .expect(topMenu.customers.innerText).match(
+    sharedFunctions.anyCase('Customers')
+    )
+})
+
+test('Check "Services" top-menu link is displayed on website header', async t => {
+  await t
+    .expect(topMenu.about.innerText).match(
+    sharedFunctions.anyCase('About Us')
+    )
+})
+
+test('Check "Services" top-menu link is displayed on website header', async t => {
+  await t
+    .expect(topMenu.contact.innerText).match(
+    sharedFunctions.anyCase('Contact')
+    )
+})
+
+test('Check "Services" top-menu link is displayed on website header', async t => {
+  await t
+    .expect(topMenu.blog.innerText).match(
+    sharedFunctions.anyCase('Blog')
+    )
+})
