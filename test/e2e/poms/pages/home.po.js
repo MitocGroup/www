fa744675c512659b3ce3473d7914cfa719d014f2
avozicov <avozicov@pycoding.biz>
2017-10-13 16:03:55 +0300

    Adding e2e scripts with links and path assertions, fixing poms class after css changes

--- test/e2e/poms/pages/home.po.js
+++ test/e2e/poms/pages/home.po.js
@@ -3,7 +3,7 @@ import { isVisible } from '../../helpers/config-import'
 
 export class HomePage {
   constructor () {
-    this.startProjectButton = Selector('body > div > div.main-page > div.top_button > button', isVisible)
+    this.startProjectButton = Selector('body > div > div.hide-mobile > div.main-page > div.top_button > button', isVisible)
     this.becomePartnerButton = Selector('#partners > div.container > div.top_button.center > button', isVisible)
   }
 }
